c768426d46577a1dcb6baededbfa736419fe4304
lyd <yanda@shenzhourong.com>
2020-12-22 15:26:23 +0800

    flink项目创建

---
+++ src/test/java/com/study/flinkdemo1/FlinkExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.flinkdemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlinkExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
